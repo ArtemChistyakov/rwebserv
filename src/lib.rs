e216d7c0fa69b1b95b0bb0418832904a59d814bf
artem.chistyakov <artem.chistyakov@nexign.com>
2022-02-21 21:02:28 +0300

    Created ThreadPool structure.

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::thread::JoinHandle;
+
+pub struct ThreadPool {
+    size: usize,
+    threads: Vec<JoinHandle<()>>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        let mut threads = Vec::with_capacity(size);
+        for i in 0..size {}
+        ThreadPool { size, threads }
+    }
+    pub fn execute<F>(&self, f: F)
+        where F: FnOnce() + Send + 'static
+    {}
+}
